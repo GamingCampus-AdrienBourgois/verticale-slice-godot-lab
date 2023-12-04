66bb47b07330ab5b78ca845350a817b21f120406
CatChow0 <thetrueshibadoggo@gmail.com>
2023-12-04 16:28:12 +0100

    N°4 - Le canvas layer avance

--- Scripts/Components/Arrow_fix_wire_hud.cs
+++ Scripts/Components/Arrow_fix_wire_hud.cs
@@ -5,29 +5,14 @@ using System.Runtime.CompilerServices;
 public partial class Arrow_fix_wire_hud : Sprite2D
 {
 
-	private Sprite2D sprite;
-	private int y = 200;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		sprite = GetNode<Sprite2D>("Arrow");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 input_direction = new(Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"), Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up"));
 		
-		input_direction = input_direction.Normalized();
-
-		if (input_direction != Vector2.Zero && sprite.Position.Y == 400)
-		{
-			y += 100;
-			sprite.Position = new Vector2(200,y);
-		} else if (sprite.Position.Y >= 400)
-		{
-			sprite.Position = new Vector2(200, 200);
-
-		}
 	}
 }
--- Scripts/Components/fix_wire.cs
+++ Scripts/Components/fix_wire.cs
@@ -6,11 +6,16 @@ public partial class fix_wire : Area2D
 
 	private bool interact = false;
 	private bool isShow = false;
-
+	private CanvasLayer fixWireHud;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		fixWireHud = GetNode<CanvasLayer>("Fix_wire_hud");
+		if (fixWireHud != null)
+		{
+			fixWireHud.Visible = isShow;
+			GD.Print("OK");
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,10 +27,16 @@ public partial class fix_wire : Area2D
 		}
 		else
 		{
-			if (Input.IsActionPressed("ui_accept"))
+			if (Input.IsActionJustPressed("ui_accept") && isShow == false)
 			{
 				GD.Print("Espace");
+				isShow = true;
+				fixWireHud.Visible = isShow;
 
+			} else if (Input.IsActionJustPressed("ui_left") && isShow == true){
+				GD.Print("A plus");
+				isShow = false;
+				fixWireHud.Visible = isShow;
 			}
 		}
 	}
