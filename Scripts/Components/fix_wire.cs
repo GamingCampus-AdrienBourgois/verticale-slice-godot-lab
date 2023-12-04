bee9debc5f811d8caf9e99ef2fb6fee0426ff733
CatChow0 <thetrueshibadoggo@gmail.com>
2023-12-04 15:46:05 +0100

    N°3 - Envie de crever sur ce canvas layer

--- Scripts/Components/fix_wire.cs
+++ Scripts/Components/fix_wire.cs
@@ -3,21 +3,39 @@ using System;
 
 public partial class fix_wire : Area2D
 {
+
+	private bool interact = false;
+	private bool isShow = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (interact == false)
+		{
+			
+		}
+		else
+		{
+			if (Input.IsActionPressed("ui_accept"))
+			{
+				GD.Print("Espace");
+
+			}
+		}
 	}
 
 	private void _on_body_entered(Node2D body)
 	{
 		if (body.IsInGroup("Player"))
 		{
-			GD.Print("Player");
+			interact = true;
+			GD.Print(interact);
 		}
 	}
 
@@ -25,8 +43,10 @@ public partial class fix_wire : Area2D
 	{
 		if (body.IsInGroup("Player"))
 		{
-			GD.Print("No Player");
+			interact = false;
+			GD.Print(interact);
 		}
 
 	}
+
 }
