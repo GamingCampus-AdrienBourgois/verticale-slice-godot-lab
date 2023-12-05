ca36c794ac4a67f22bdcff0bbcd66aefb8de00d1
Cookie <yumzuko@gmail.com>
2023-12-05 09:13:50 +0100

    Mini Yannick

--- Platformer/scripts/level_platformer.cs
+++ Platformer/scripts/level_platformer.cs
@@ -13,6 +13,8 @@ public partial class level_platformer : Node
 		}
 	}
 
+	// Faire un poutit script pour faire varier la speed du player
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
