bbbb463919e59da7b987ddc00772861f6f741d90
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2023-12-11 17:21:43 +0100

    bizarre le push (trier les fichier parce que double)

---
+++ Scripts/Components/Menu_cursor.cs
@@ -0,0 +1,148 @@
+using Godot;
+using System;
+
+public partial class Menu_cursor : TextureRect
+{
+	[Export]
+	public NodePath nodePath;
+	[Export]
+	public Vector2 cursorOffset;
+
+	private Fix_wire_hud fixWireHud;
+
+	private Line2D line;
+
+	private int cursorIndex = 0;
+	private bool isWireSelected = false;
+	private bool colorMatched;
+	private Vector2 wireSelected = Vector2.Zero;
+	GridContainer menu_parent;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		menu_parent = GetNode(nodePath) as GridContainer;
+		fixWireHud = (Fix_wire_hud)GetParent();
+
+		CallDeferred("DeferredInitialization");
+	}
+
+	private void DeferredInitialization()
+	{
+		// Le reste de votre initialisation...
+		line = new Line2D();
+		GetParent().AddChild(line);
+
+		line.DefaultColor = new Color(1, 1, 1);
+		line.Width = 2.0f;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 input = Vector2.Zero;
+
+		if (Input.IsActionJustPressed("Up"))
+		{
+			input.Y -= 1;
+		}
+		if (Input.IsActionJustPressed("Down"))
+		{
+			input.Y += 1;
+		}
+
+		if (menu_parent is GridContainer)
+		{
+			SetCursorFromIndex(cursorIndex + (int)input.Y * menu_parent.Columns);
+		}
+
+		if (Input.IsActionJustPressed("Accept") && isWireSelected == false) 
+		{
+			wireSelected.X = cursorIndex;
+			SetCursorFromIndex(cursorIndex + 1);
+			isWireSelected = true;
+		}
+		else if (Input.IsActionJustPressed("Accept") && isWireSelected == true)
+		{
+			wireSelected.Y = cursorIndex;
+			colorMatched = CheckWiresColor(wireSelected);
+
+			UpdateLine();
+		}
+	}
+
+	public Control GetMenuItemAtIndex(int index)
+	{
+		if (menu_parent == null) {
+			return null;
+		}
+
+		if (index >= menu_parent.GetChildCount() || index < 0)
+		{
+			return null;
+		}
+
+		return menu_parent.GetChild(index) as Control;
+
+	}
+
+	public void SetCursorFromIndex(int index)
+	{
+		var menu_item = GetMenuItemAtIndex(index);
+
+		if (menu_item == null)
+		{
+			return;
+		}
+
+		Vector2 Position = menu_item.GlobalPosition;
+		Vector2 Size = menu_item.Size;
+
+		GlobalPosition = new Vector2(Position.X, (Position.Y - Size.Y / 2.0f)) - (Size / 2.0f) - cursorOffset;
+
+		cursorIndex = index;
+
+	}
+
+	private bool CheckWiresColor(Vector2 wire) 
+	{
+		var itemLeft = GetMenuItemAtIndex((int)wire.X) as ColorRect;
+		var itemRight = GetMenuItemAtIndex((int)wire.Y) as ColorRect;
+
+		if (itemLeft == null || itemRight == null)
+		{
+			return false;
+		}
+
+		if (itemLeft.Color == itemRight.Color) 
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	private void UpdateLine() 
+	{
+		var startMenuItem = GetMenuItemAtIndex((int)wireSelected.X);
+		var endMenuItem = GetMenuItemAtIndex((int)wireSelected.Y);
+
+		if (startMenuItem == null || endMenuItem == null)
+		{
+			return;
+		}
+
+		var startPos = -startMenuItem.GlobalPosition + startMenuItem.Size / 2.0f;
+		var endPos = -endMenuItem.GlobalPosition + endMenuItem.Size / 2.0f;
+		GD.Print(startMenuItem.GlobalPosition + " " + endMenuItem.GlobalPosition);
+
+		// Effacez tous les points actuels de la ligne
+		line.ClearPoints();
+
+		// Ajoutez les deux points à la ligne
+		line.AddPoint(startPos);
+		line.AddPoint(endPos);
+	}
+}
--- Scripts/Components/fix_wire.cs
+++ Scripts/Components/fix_wire.cs
@@ -1,13 +1,13 @@
 using Godot;
 using System;
 
-public partial class FixWire : Area2D
+public partial class fix_wire : Area2D
 {
 	private Player player;
 	private CanvasLayer fixWireHud;
-	private MenuCursor menuCursor;
-	private bool interact = false;
+	private bool Interact = false;
 	private bool isShow = false;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,24 +15,29 @@ public partial class FixWire : Area2D
 
 		player = GetParent().GetNode<Player>("Player");
 		fixWireHud = GetNode<CanvasLayer>("Fix_wire_hud");
-		menuCursor = fixWireHud.GetNode<MenuCursor>("Menu_cursor");
 		if (fixWireHud != null)
 		{
 			fixWireHud.Visible = isShow;
+			
 		}
+		GD.Print(fixWireHud.Visible, "     ", fixWireHud);
 	}
 	
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (interact == true)
+		if (Interact == false)
 		{
-			if (Input.IsActionJustPressed("Interact") && isShow == false)
+			
+		}
+		else
+		{
+			if (Input.IsActionJustPressed("Accept") && isShow == false)
 			{
 				isShow = true;
 				player.inputOnFocus = true;
 				fixWireHud.Visible = isShow;
-				menuCursor.isWiring = true;
 
 			} else if (Input.IsActionJustPressed("ui_cancel") && isShow == true)
 			{
@@ -43,25 +48,11 @@ public partial class FixWire : Area2D
 		}
 	}
 
-	private void ShowText()
-	{
-		if (interact == true)
-		{
-			player.to_label.Text = "E to interact";
-			player.ui_animations.Play("appear");
-		}
-		else if (interact == false)
-		{
-			player.ui_animations.PlayBackwards("appear");
-		}
-	}
-
 	private void _on_body_entered(Node2D body)
 	{
 		if (body.IsInGroup("Player"))
 		{
-			interact = true;
-			ShowText();
+			Interact = true;
 		}
 	}
 
@@ -69,18 +60,9 @@ public partial class FixWire : Area2D
 	{
 		if (body.IsInGroup("Player"))
 		{
-			interact = false;
-			ShowText();
+			Interact = false;
 		}
 
-	}
-
-	public void WireIsFix()
-	{
-		isShow = false;
-		player.inputOnFocus = false;
-		fixWireHud.Visible = isShow;
-		GD.Print("Cable Reparer");
 	}
 
 }
