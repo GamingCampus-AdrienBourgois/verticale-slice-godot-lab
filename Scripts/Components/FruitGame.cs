28a82d4d6a20a6f16a94e30560e62d515eecc467
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2023-12-13 21:00:12 +0100

    Fruit game finished

---
+++ Scripts/Components/FruitGame.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+using System.Data;
+
+public partial class FruitGame : Node
+{
+	public bool STATE = false;
+	public override void _Ready()
+	{
+		foreach (Node i in GetNode<Node>("Areas").GetChildren())
+		{
+			i.Connect("ON",new Callable(this,"FruitON"));
+			i.Connect("OFF",new Callable(this,"FruitOFF"));
+		}
+	}
+
+	private void FruitON(ObjectFixArea Area, Node2D body)
+	{
+		if(Area.IsInGroup("True"))
+		{
+			if(body.IsInGroup("Kiwi"))
+			{
+				GD.Print("Porte active");
+				// Fait la porte
+				if(body.IsInGroup("True"))
+				{
+					GD.Print("Valid");
+					STATE = true;
+				}
+			}
+		}
+	}
+
+	private void FruitOFF(ObjectFixArea Area, Node2D body)
+	{
+		if(Area.IsInGroup("True"))
+		{
+			if(body.IsInGroup("Kiwi"))
+			{
+				GD.Print("Porte desactivé");
+				// Fait la porte
+				if(body.IsInGroup("True"))
+				{
+					GD.Print("not valid");
+					STATE = false;
+				}
+			}
+		}
+
+	}
+
+
+}
